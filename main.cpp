e5b5f0239d761d2f748b09ef5af9daf102fb90b1
Aspric <1394854425@qq.com>
2025-07-26 14:54:20 +0800

    draw triangle

--- main.cpp
+++ main.cpp
@@ -6,6 +6,12 @@
 
 void onResize(int width, int height);
 void onKey(int key, int scancode, int action, int mods);
+void testShader();
+void testBindInterleavedBuffer();
+void render();
+
+GLint program;
+GLuint vao;
 
 int main() {
 
@@ -24,12 +30,14 @@ int main() {
 	glViewport(0, 0, 800, 600);
 	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
-
+	testShader();
+	testBindInterleavedBuffer();
 
 	// 窗体循环
 	while (app->update()) {
-		// 画布清理
-		glClear(GL_COLOR_BUFFER_BIT);
+
+		render();
+
 	}
 
 	app->destory();
@@ -85,7 +93,7 @@ void testBindSingleBuffer() {
 	glBufferData(GL_ARRAY_BUFFER, sizeof(color), color, GL_STATIC_DRAW);
 
 	//生成vao
-	GLuint vao = 0;
+	vao = 0;
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
 
@@ -119,6 +127,7 @@ void testBindInterleavedBuffer() {
 		-0.5f,-0.5f,0.0f,1.0f,0.0f,0.0f,
 		0.5f,-0.5f,0.0f,0.0f,1.0f,0.0f,
 		0.0f,0.5f,0.0f,0.0f,0.0f,1.0f,
+		1.0f,0.3f,0.6f,0.0f,0.0f,1.0f,
 	};
 
 
@@ -132,7 +141,7 @@ void testBindInterleavedBuffer() {
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 	//生成vao
-	GLuint vao = 0;
+	vao = 0;
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
 
@@ -145,4 +154,77 @@ void testBindInterleavedBuffer() {
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
 
 	glBindVertexArray(0);//将当前的vao解绑
+}
+
+void testShader() {
+	const char* vertexShaderSource = 
+		"#version 460 core\n"
+		"layout (location = 0) in vec3 aPos;\n"
+		"void main()\n"
+		"{\n"
+		"	gl_Position = vec4(aPos.x,aPos.y,aPos.z,1.0);\n"
+		"}\0";
+	const char* fragmentShaderSource = 
+		"#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"void main()\n"
+		"{\n"
+		"	FragColor = vec4(1.0f,0.5f,0.2f,1.0);\n"
+		"}\0";
+	// 创建vs和fs的shader程序
+	GLuint vertex, fragment;//程序的句柄
+	vertex = glCreateShader(GL_VERTEX_SHADER);
+	fragment = glCreateShader(GL_FRAGMENT_SHADER);
+	// 为shader程序输入源码
+	glShaderSource(vertex, 1, &vertexShaderSource, NULL);//因为source是\0结尾的，无需指定长度
+	glShaderSource(fragment, 1, &fragmentShaderSource, NULL);//因为source是\0结尾的，无需指定长度
+	// 编译shader
+	int isSuccess = 0;
+	char infoLog[1024];
+	glCompileShader(vertex);
+	glGetShaderiv(vertex,GL_COMPILE_STATUS,&isSuccess);
+	if (!isSuccess) {
+		glGetShaderInfoLog(vertex, 1024, NULL, infoLog);
+		std::cout << "编译顶点着色器出错:\n" << infoLog << "\n";
+	}
+	glCompileShader(fragment);
+	glGetShaderiv(fragment, GL_COMPILE_STATUS, &isSuccess);
+	if (!isSuccess) {
+		glGetShaderInfoLog(fragment, 1024, NULL, infoLog);
+		std::cout << "编译片段着色器出错:\n" << infoLog << "\n";
+	}
+	// 创建一个program程序
+	program = 0;
+	program = glCreateProgram();
+	// 编译好的fs vs放入program
+	glAttachShader(program, vertex);
+	glAttachShader(program, fragment);
+	// program链接生成可执行程序
+	glLinkProgram(program);
+	glGetProgramiv(program, GL_LINK_STATUS, &isSuccess);
+	if (!isSuccess) {
+		glGetProgramInfoLog(program, 1024, NULL, infoLog);
+		std::cout << "着色器链接出错:\n" << infoLog << "\n";
+	}
+	// 清理
+	glDeleteShader(vertex);
+	glDeleteShader(fragment);
+}
+
+void render() {
+	// 画布清理
+	glClear(GL_COLOR_BUFFER_BIT);
+	// 绑定program
+	glUseProgram(program);
+	// 绑定vao
+	glBindVertexArray(vao);
+	// 绘制
+	/* 有点 0 1 2 3 4 5 6   
+	* GL_TRIANGLES 0 1 2|3 4 5 不足的不绘制
+	* GL_TRIANGLE_STRIP  0 1 2| 2 1 3|2 3 4|4 3 5   一个三角形最后一个Index为n n为奇数 [n-2,n-1,n] n为偶数 [n-1,n-2,n](总会保持逆时针的顺序)
+	* GL_TRIANGLE_FAN 扇形绘制 0 1 2|0 2 3|0 3 4|0 5 6
+	* GL_LINES 0 1|2 3|3 4|4 5|5 6
+	* GL_LINES_STRIP 0 1|1 2|2 3|3 4|4 5|5 6
+	*/
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 }
